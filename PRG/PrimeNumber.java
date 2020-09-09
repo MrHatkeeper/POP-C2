0e10f9a5c1b386250c253a0c8900965e2baece92
MrHatkeeper <58586406+MrHatkeeper@users.noreply.github.com>
2020-09-09 16:01:43 +0200

    Update PrimeNumber.java

--- PRG/PrimeNumber.java
+++ PRG/PrimeNumber.java
@@ -1,18 +1,17 @@
-public class PrimeNumber {
+public class NonPrimeNumber {
     public static void main(String []args){
 
-        String prime = "";
-        for(int i = 1; i < 101; i++){
+
+        for(int i = 2; i < 101; i++){
             int count = 0;
-            for(int j = i; j > 2; j--){
-                if(i%j == 0){
+            for(int j = 1; j < i; j++){
+                if(i % j == 0){
                     count++;
                 }
             }
-            if(count == 2){
-                prime += i + " ";
+            if(count == 1){
+                System.out.println(i);
             }
         }
-        System.out.println(prime);
     }
 }
