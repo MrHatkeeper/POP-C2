9b351fc1ef93798159970369d062d97ebdd03078
MrHatkeeper <58586406+MrHatkeeper@users.noreply.github.com>
2020-09-21 16:19:13 +0200

    Update OpakovaniDveJedna.java

--- PRG/Opakovani2/OpakovaniDveJedna.java
+++ PRG/Opakovani2/OpakovaniDveJedna.java
@@ -4,7 +4,7 @@ public class OpakovaniDveJedna {
     public static void main(String []args){
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Jaká si věta");
+        System.out.println("Jakási věta");
         String[] arr = sc.nextLine().split(" ");
 
         System.out.println(arr[arr.length - 2]);
